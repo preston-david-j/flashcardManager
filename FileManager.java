03755a2e58fa13004ec92341bf1aff7f03324e06
prestondj <prestondjohnson2006@gmail.com>
2025-06-21 22:32:16 +0100

    Added temporary SCC.121 file for debugging, and removed Debug statements.

--- FileManager.java
+++ FileManager.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class FileManager 
 {
     public static String[] directories;
-    
+
     public static void InitFileManager(){
         File folder = new File("data");
         File[] childDirectories = folder.listFiles(File::isDirectory);
@@ -12,7 +12,5 @@ public class FileManager
         for(int i = 0 ; i < childDirectories.length; i++){
             directories[i] = childDirectories[i].getName();
         }
-        
-        Debug.printArray(directories);
     }
 }
